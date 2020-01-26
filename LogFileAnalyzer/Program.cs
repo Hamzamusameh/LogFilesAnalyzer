c35ee7b0a6c977a873a872430cd819ec095d8380
Hamza Musameh <hamza.musameh@hotmail.com>
2020-01-26 22:03:46 +0200

    Init Log file analyzer

--- LogFileAnalyzer/Program.cs
+++ LogFileAnalyzer/Program.cs
@@ -1,21 +1,108 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace LogFileAnalyzer
+namespace LogFilesReader
 {
+    class MyPair
+    {
+        public string key;
+        public int value;
+        public MyPair(string key, int value)
+        {
+            this.value = value;
+            this.key = key;ghfgh
+        }
+    }
+
+    class Comparer : IComparer<MyPair>
+    {
+        public int Compare(MyPair x, MyPair y)
+        {
+            return y.value.CompareTo(x.value);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string line;
+            string[] nth = new string[5] { "first", "second", "third", "fourth", "fifth" };
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            HashSet<string> users = new HashSet<string>();
+            HashSet<string> UniqueRecords = new HashSet<string>();
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            List<MyPair> listOfSortedResult = new List<MyPair>();
+
+            Comparer comparer = new Comparer();
+
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("D:\\trackFile2015-03-08.log");
+
+                //Read the first line of text
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    //Remove DateTime from string
+                    line = line.Substring(25);
+
+                    //Split cleared line into User , Operation
+                    var splittedLine = line.Split('/');
+
+                    users.Add(splittedLine[0]);
+
+                    UniqueRecords.Add(line);
+
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //Count how much every operation has been occured
+                foreach (string item in UniqueRecords)
+                {
+                    string operation = item.Split('/')[1];
+                    if (dictionary.ContainsKey(operation))
+                        dictionary[operation]++;
+                    else
+                        dictionary.Add(operation, 1);
+                }
+
+                //Fill items in dictionary in list to be accessible by index and sorting easily
+                foreach (var item in dictionary)
+                {
+                    MyPair temp = new MyPair(item.Key, item.Value);
+                    listOfSortedResult.Add(temp);
+                }
+
+                //Sort the list in descending order
+                listOfSortedResult.Sort(comparer);
+
+                //Print The Result based on number of operations
+                Console.Write("Please Enter Number of operations: ");
+                int numberOfOperationNeeded = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numberOfOperationNeeded; i++)
+                    Console.WriteLine($"operation '{listOfSortedResult[i].key}' is the most {nth[i]} common and is used by {(listOfSortedResult[i].value / Convert.ToDouble(users.Count)) * 100}% of our users");
+
+                //Close the file
+                sr.Close();
+
+                //Remain Console Opened
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
